a3ef84277854f5e9978d855207b4280be71e6b1c
Therealankitk <ankitkundu2005@gmail.com>
2025-06-19 03:07:22 +0530

    bugs fixes

--- src/hangdata.js
+++ src/hangdata.js
@@ -1,30 +1,17 @@
+import hang0 from './assets/hang0.png';
+import hang1 from './assets/hang1.png';
+import hang2 from './assets/hang2.png';
+import hang3 from './assets/hang3.png';
+import hang4 from './assets/hang4.png';
+import hang5 from './assets/hang5.png';
+import hang6 from './assets/hang6.png';
+
 export default [
-    {
-        "id": 0,
-        "src" : "/src/assets/hang0.png"    
-    },
-    {
-        "id": 1,
-        "src" : "/src/assets/hang1.png"    
-    },
-    {
-        "id": 2,
-        "src" : "/src/assets/hang2.png"    
-    },
-    {
-        "id": 3,
-        "src" : "/src/assets/hang3.png"    
-    },
-    {
-        "id": 4,
-        "src" : "/src/assets/hang4.png"    
-    },
-    {
-        "id": 5,
-        "src" : "/src/assets/hang5.png"    
-    },
-    {
-        "id": 6,
-        "src" : "/src/assets/hang6.png"    
-    },
-]
+    { id: 0, src: hang0 },
+    { id: 1, src: hang1 },
+    { id: 2, src: hang2 },
+    { id: 3, src: hang3 },
+    { id: 4, src: hang4 },
+    { id: 5, src: hang5 },
+    { id: 6, src: hang6 },
+];
